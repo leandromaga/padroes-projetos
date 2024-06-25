0e53228cfe645b2e46b71e4068e4d73b6b6ae097
Rafael Oliveira <reis.r3@icloud.com>
2024-06-24 22:36:37 -0300

    adicionando mediator

--- src/test/java/padroesComportamentais/mediator/EventosMediatorTest.java
+++ src/test/java/padroesComportamentais/mediator/EventosMediatorTest.java
@@ -2,6 +2,7 @@ package padroesComportamentais.mediator;
 
 import org.junit.jupiter.api.Test;
 import padroesProjeto.padroesComportamentais.mediator.CoordenadorEventos;
+import padroesProjeto.padroesComportamentais.mediator.Evento;
 import padroesProjeto.padroesComportamentais.mediator.Participante;
 import padroesProjeto.padroesComportamentais.mediator.SuporteTecnico;
 
@@ -9,7 +10,6 @@ import static org.junit.Assert.assertEquals;
 
 public class EventosMediatorTest {
 
-    // Testes para Participante
     @Test
     void deveIniciarParticipacao() {
         Participante participante = new Participante();
@@ -28,7 +28,6 @@ public class EventosMediatorTest {
                 participante.encerrarParticipacao("Desmontagem completa"));
     }
 
-    // Testes para CoordenadorEventos
     @Test
     void deveCoordenarInicio() {
         CoordenadorEventos coordenador = CoordenadorEventos.getInstancia();
@@ -47,7 +46,6 @@ public class EventosMediatorTest {
                 coordenador.coordenarEncerramento("Limpando área"));
     }
 
-    // Testes para SuporteTecnico
     @Test
     void devePrepararEvento() {
         SuporteTecnico suporte = SuporteTecnico.getInstancia();
@@ -60,5 +58,22 @@ public class EventosMediatorTest {
         SuporteTecnico suporte = SuporteTecnico.getInstancia();
         assertEquals("O Suporte Técnico desmontou o local após o evento: Retirada de equipamentos",
                 suporte.desmontarEvento("Retirada de equipamentos"));
+    }
+    @Test
+    void deveIniciarParticipacaoComoEvento() {
+        Evento evento = new Evento();
+        assertEquals("O Coordenador de Eventos iniciou o evento.\n" +
+                        "O Suporte Técnico preparou tudo conforme o necessário.\n" +
+                        ">>O Suporte Técnico preparou o local para o evento: Preparativos completos",
+                evento.iniciarParticipacao("Preparativos completos"));
+    }
+
+    @Test
+    void deveEncerrarParticipacaoComoEvento() {
+        Evento evento = new Evento();
+        assertEquals("O Coordenador de Eventos encerrou o evento.\n" +
+                        "O Suporte Técnico desmontou a estrutura conforme necessário.\n" +
+                        ">>O Suporte Técnico desmontou o local após o evento: Desmontagem completa",
+                evento.encerrarParticipacao("Desmontagem completa"));
     }
 }
